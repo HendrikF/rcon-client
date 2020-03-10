624ab453da551edea0619b1492c1c5250264cc51
Hendrik Fritsch <hendrik.fritsch@web.de>
2020-03-10 20:16:56 +0100

    Add command discovery and completion

---
+++ README.md
@@ -0,0 +1,12 @@
+# RCON Client
+
+A client implementation of the Source RCON Protocol.
+
+The RCON Protocol allows remote command execution in game servers that implement it.
+
+NOTE: Command completion and discovery is specialized to the command syntax of Minecraft, but all game specific parts are separated from the core.
+
+See https://developer.valvesoftware.com/wiki/Source_RCON_Protocol
+and https://wiki.vg/RCON
+
+MIT License
--- client.py
+++ client.py
@@ -5,10 +5,13 @@ import atexit
 import os
 import rlcompleter
 import readline
+import re
 
 from rcon.rcon import Connection
 
-help_output = None
+commands_learned = {}
+
+debug = (lambda *args, **kw: print(*args, **kw, file=sys.stderr, flush=True)) if len(sys.argv) >= 2 and sys.argv[1] == '--debug' else (lambda *args, **kw: None)
 
 # config
 
@@ -42,12 +45,119 @@ atexit.register(save_hist, hist_len, hist_file)
 # auto completion
 
 readline.parse_and_bind('tab: complete')
+# removed <>
+readline.set_completer_delims(''' \t\n`~!@#$%^&*()-=+[{]}\|;:'",/?''')
+
+debug('== RESTART ==')
+
 def completer(text, state):
-    if help_output is None: return None
-    commands = [command.strip('/') for command in help_output.split('\n')]
-    commands = list(filter(lambda command: command.startswith(text), commands))
-    return commands[state]
+    # text will be the current word to complete
+    line_buffer = readline.get_line_buffer()
+    begin = readline.get_begidx()
+    end = readline.get_endidx()
+
+    parts_before_cursor = re.split('\s+', line_buffer[:begin].rstrip(' '))
+    if len(parts_before_cursor) == 1 and parts_before_cursor[0] == '':
+        parts_before_cursor = []
+    debug(parts_before_cursor, text, state)
+
+    # complete help with actual commands
+    if len(parts_before_cursor) >= 1 and parts_before_cursor[0] == 'help':
+        parts_before_cursor.pop(0)
+
+    d = commands_learned
+    for part in parts_before_cursor:
+        d = d.get(part, None)
+        if d is None:
+            break
+
+    if d is None:
+        # unknown command started before cursor
+        return None
+
+    parts_possible = list(filter(lambda part: part.startswith(text), d.keys()))
+
+    if len(parts_possible) == 1 and state == 0:
+        readline.insert_text(' ')
+        debug('SPACE')
+
+    return parts_possible[state]
+
 readline.set_completer(completer)
+
+def peek_iter(iterable):
+    """ provides access to 2 following elements of an iterable
+
+        for current, next1, next2 in peek_iter(iterable): ...
+    """
+    l = [*iterable, None, None]
+    return zip(l, l[1:], l[2:])
+
+def learn_commands(help_output):
+    """ populates the commands_learned dict
+
+        the goal is a structure like this:
+        {
+          "data": {
+            "get": {
+              "block": {},
+              "entity": {}
+            },
+            "modify": {
+              "block": {},
+              "entity": {}
+            }
+          },
+          "help": {
+            "<command>": {}
+          }
+        }
+
+        which would mean that the following commands are known:
+        /data get block
+        /data get entity
+        /data modify block
+        /data modify entity
+        /help <command>
+
+        command aliases are created by referencing the target dict from the alias
+    """
+    for line in help_output.split('\n'):
+        if line.startswith('/'):
+            parts = re.split('\s+', line.lstrip('/'))
+            d = commands_learned
+            # use our peek_iter to have a look at the following parts
+            # we need this to detect aliases
+            for part, next1, next2 in peek_iter(parts):
+                if next1 == '->':
+                    # alias
+                    # create and get alias target, because it might not exist yet
+                    target = commands_learned.setdefault(next2, {})
+                    d.setdefault(part, target)
+                    break
+                elif part.startswith('(') and part.endswith(')'):
+                    # choice (x|y)
+                    choices = part[1:-1].split('|')
+                    for choice in choices:
+                        d.setdefault(choice, {})
+                    # we cannot continue, choices have to be at the end of commands
+                    # (we cannot decide which choice to append following parts to)
+                    break
+                elif part.startswith('[') and part.endswith(']'):
+                    # optional choice [x|y]
+                    choices = part[1:-1].split('|')
+                    if len(choices) == 1:
+                        # not a choice, just an option
+                        d = d.setdefault(choices.pop(0), {})
+                    else:
+                        for choice in choices:
+                            d.setdefault(choice, {})
+                        # we cannot continue, choices have to be at the end of commands
+                        # (we cannot decide which choice to append following parts to)
+                        break
+                else:
+                    d = d.setdefault(part, {})
+    debug('knowledge', commands_learned)
 
 # connection
 
@@ -72,8 +182,7 @@ try:
         if command.startswith('help'):
             # insert line breaks into help output
             result = '\n/'.join(result.split('/'))
-        if command == 'help':
-            help_output = result
+            learn_commands(result)
         print(result)
 except (EOFError, KeyboardInterrupt):
     print('')
